97b03e6ef02c964fad0feb3ff0d9c83f7403454e
o-masny <icelyy@seznam.cz>
2023-11-25 23:36:49 +0100

    feat: :art: location service

---
+++ meteoapp/lib/services/location_service.dart
@@ -0,0 +1,31 @@
+// location_service.dart
+import 'package:location/location.dart';
+
+class LocationService {
+  Location location = Location();
+
+  Future<bool> requestPermission() async {
+    bool serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return false;
+      }
+    }
+
+    PermissionStatus permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  Future<LocationData?> getLocation() async {
+    return await location.getLocation();
+  }
+}
+//
